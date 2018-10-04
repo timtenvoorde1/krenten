d452a21260ff64416d97b7d031fbbe219c3f8df0
SamVerzijde <43599153+SamVerzijde@users.noreply.github.com>
2018-10-04 15:27:13 +0200

    Style sheet aangepast
    Div's moeten in de index.php worden gezet.
    
    Robot plaatje zit in een box in het midden, de chatbox heeft een eigen box gekregen.

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<title>Chatbox</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<h1>Chatbot</h1>
+		<div id="chatbox">
+		</div>
+		<div id="chatbotlogo">
+			<img src="chatbot2.png" alt="chatbotlogo">
+		</div>
+		<input type="text" name="chatbox">
+	</body>
+</html>
